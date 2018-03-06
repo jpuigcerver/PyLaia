238ceac8348743773d62f9ffbf3d2d7126ade358
jpuigcerver <joapuipe@gmail.com>
2018-03-06 01:09:00 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, find_packages
 
 setup(name='laia', version='0.1', packages=find_packages(),
       scripts=[],
-      install_requires=['editdistance', 'torch', 'numpy', 'Pillow',
-                        'mock;python_version<"3.0"'],
+      install_requires=['editdistance',
+                        'future;python_version<"3.0"',
+                        'mock;python_version<"3.0"',
+                        'numpy',
+                        'torch',
+                        'Pillow'],
       extras_require={
           'progress_bar': ['tqdm'],
       },
