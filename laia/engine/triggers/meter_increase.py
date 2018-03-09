acc751879d61ccc99b458725018018cd2aafd20e
jpuigcerver <joapuipe@gmail.com>
2018-03-09 05:05:42 +0100

    Refactored triggers reading from meters

--- laia/engine/triggers/meter_increase.py
+++ laia/engine/triggers/meter_increase.py
@@ -2,39 +2,22 @@ from __future__ import absolute_import
 
 import logging
 
-from laia.engine.triggers.trigger import LoggedTrigger, TriggerLogWrapper
+from laia.engine.triggers.trigger import TriggerLogWrapper
+from laia.engine.triggers.trigger_from_meter import TriggerFromMeter
 from laia.meters.meter import Meter
+
+from typing import Any
 
 _logger = logging.getLogger(__name__)
 
 
-class MeterIncrease(LoggedTrigger):
+class MeterIncrease(TriggerFromMeter):
     def __init__(self, meter, meter_key=None, name=None):
-        # type: (Meter, str) -> None
-        assert isinstance(meter, Meter)
-        super(MeterIncrease, self).__init__(_logger, name)
-        self._meter = meter
-        self._meter_key = meter_key
+        # type: (Meter, Any, str) -> None
+        super(MeterIncrease, self).__init__(meter, _logger, meter_key, name)
         self._highest = float('-inf')
 
-    def __call__(self):
-        # Try to get the meter's last value, if some exception occurrs,
-        # we assume that the meter has not produced any value yet, and
-        # we do not trigger.
-        try:
-            last_value = self._meter.value
-            if last_value is None:
-                raise TypeError('Meter returned None')
-        except Exception:
-            self.logger.exception(
-                TriggerLogWrapper(self, 'No value fetched from meter'))
-            return False
-
-        if self._meter_key is not None:
-            last_value = last_value[self._meter_key]
-
-        # Return True, iff the value read from the meter is higher than the
-        # highest value seen so far.
+    def _process_value(self, last_value):
         if last_value > self._highest:
             self.logger.info(
                 TriggerLogWrapper(
