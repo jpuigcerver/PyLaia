2e86e7af50addcb5edaaa39e6fea97d6257c249b
jpuigcerver <joapuipe@gmail.com>
2018-03-09 02:33:11 +0100

    Change default logging level to INFO

--- laia/utils/logging.py
+++ laia/utils/logging.py
@@ -6,5 +6,6 @@ BASIC_FORMAT = '%(asctime)s %(levelname)s %(name)s : %(message)s'
 DETAILED_FORMAT = '%(asctime)s %(levelname)s %(name)s [%(pathname)s:%(lineno)d] : %(message)s'
 
 
-def init(fmt=None):
-    logging.basicConfig(format=BASIC_FORMAT if fmt is None else fmt)
+def init(fmt=None, level=logging.INFO):
+    logging.basicConfig(format=BASIC_FORMAT if fmt is None else fmt,
+                        level=level)
