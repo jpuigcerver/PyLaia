e489d384e157ee499529958d3d8572eff555beb9
carmocca <carlossmocholi@gmail.com>
2018-03-16 15:59:38 +0100

    Minor changes

--- laia/losses/__init__.py
+++ laia/losses/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
 
+import laia.plugins.logging as log
+
 try:
     from laia.losses.ctc_loss import CTCLoss
 except:
-    # TODO(jpuigcerver): Show some kind of warning, since CTC is probably a key component!
-    pass
+    log.basic_config()
+    log.warning('Missing CTC loss function library')
--- laia/plugins/__init__.py
+++ laia/plugins/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import absolute_import
 
 import laia.plugins.logging
+import laia.plugins.arguments_types
+import laia.plugins.arguments
