4cf63eae29d001e4b9910082720e705ad75cd2c3
carmocca <carlossmocholi@gmail.com>
2018-03-16 19:16:32 +0100

    Start state saving functions

--- laia/losses/__init__.py
+++ laia/losses/__init__.py
@@ -5,5 +5,4 @@ import laia.plugins.logging as log
 try:
     from laia.losses.ctc_loss import CTCLoss
 except:
-    log.basic_config()
     log.warning('Missing CTC loss function library')
--- laia/plugins/__init__.py
+++ laia/plugins/__init__.py
@@ -3,3 +3,4 @@ from __future__ import absolute_import
 import laia.plugins.logging
 import laia.plugins.arguments_types
 import laia.plugins.arguments
+import laia.plugins.saver
