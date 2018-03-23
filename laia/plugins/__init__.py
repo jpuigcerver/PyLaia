9d2b50447c311c39ae713fb74680cdee22f1f609
carmocca <carlossmocholi@gmail.com>
2018-03-23 20:43:50 +0100

    Savers and loaders working

--- laia/plugins/__init__.py
+++ laia/plugins/__init__.py
@@ -1,12 +1,13 @@
 from __future__ import absolute_import
 
-import laia.plugins.arguments_types
 import laia.plugins.arguments
-"""
+import laia.plugins.arguments_types
+from laia.plugins.loader import (ModelLoader,
+                                 ModelCheckpointLoader,
+                                 TrainerCheckpointLoader)
 from laia.plugins.saver import (ModelSaver,
                                 ModelCheckpointSaver,
                                 TrainerCheckpointSaver,
                                 LastCheckpointsSaver)
-"""
 from laia.plugins.saver_trigger import (SaverTrigger,
                                         SaverTriggerCollection)
