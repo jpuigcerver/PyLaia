37940a3079bb6fbab3dd7d94efa86de0e5e10171
carmocca <carlossmocholi@gmail.com>
2018-04-05 18:27:15 +0200

    Use inspect to extract func arguments. Add tests

--- laia/hooks/hook.py
+++ laia/hooks/hook.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import inspect
 from functools import wraps
 from typing import Callable, Tuple
 
@@ -39,15 +40,29 @@ class HookCollection(object):
 
 
 def action(func):
-    """
-    Decorator. Filters all kwargs passed which
-    are not part of the function's parameters
+    """Decorator.
+
+    Filters the number of arguments passed using
+    the number of non kwarg arguments in the signature.
+
+    Also filters all kwargs passed which
+    are not part of the function parameters
+
+    Note: It does not make sense to take `*args, **kwargs`
+    in an `@action` annotated function since any possible
+    value passed will be filtered by the wrapper.
     """
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        return func(*args,
+        try:
+            argspec = inspect.getfullargspec(func)
+        except AttributeError:
+            argspec = inspect.getargspec(func)
+
+        non_kwargs_num = len(argspec.args) - len(argspec.defaults or [])
+        return func(*args[:non_kwargs_num],
                     **{k: v for k, v in kwargs.items()
-                       if k in func.__code__.co_varnames})
+                       if k in argspec.args})
 
     return wrapper
