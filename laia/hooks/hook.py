ff6854e4c87cca01d471c1cd64631b8d5b785d7c
carmocca <carlossmocholi@gmail.com>
2018-04-06 18:37:13 +0200

    Move action out of the hooks file

--- laia/hooks/hook.py
+++ laia/hooks/hook.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import
 
-import inspect
-from functools import wraps
 from typing import Callable, Tuple
 
 
@@ -37,32 +35,3 @@ class HookCollection(object):
 
     def __call__(self, *args, **kwargs):
         return any(h(*args, **kwargs) for h in self._hooks)
-
-
-def action(func):
-    """Decorator.
-
-    Filters the number of arguments passed using
-    the number of non kwarg arguments in the signature.
-
-    Also filters all kwargs passed which
-    are not part of the function parameters
-
-    Note: It does not make sense to take `*args, **kwargs`
-    in an `@action` annotated function since any possible
-    value passed will be filtered by the wrapper.
-    """
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        try:
-            argspec = inspect.getfullargspec(func)
-        except AttributeError:
-            argspec = inspect.getargspec(func)
-
-        non_kwargs_num = len(argspec.args) - len(argspec.defaults or [])
-        return func(*args[:non_kwargs_num],
-                    **{k: v for k, v in kwargs.items()
-                       if k in argspec.args})
-
-    return wrapper
