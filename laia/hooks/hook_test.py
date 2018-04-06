734e9fa1699d6bbfaee35906eb64582f2f579a3b
carmocca <carlossmocholi@gmail.com>
2018-04-06 18:37:13 +0200

    Move action out of the hooks file

--- laia/hooks/hook_test.py
+++ laia/hooks/hook_test.py
@@ -85,31 +85,5 @@ class HookTest(unittest.TestCase):
         self.assertFalse(Hook(F, action_with_kwargs)(v='X'))
 
 
-class ActionTest(unittest.TestCase):
-
-    def test_normal_usage(self):
-        self.assertEqual('X', action_with_arg('X'))
-        self.assertEqual('X', action_with_kwargs(v='X'))
-        self.assertTrue(empty_action())
-
-    def test_with_extra_args(self):
-        self.assertEqual('X', action_with_arg('X', 'Y'))
-        self.assertTrue(empty_action('X', 'Y'))
-
-    def test_with_extra_kwargs(self):
-        self.assertTrue(empty_action('X', v='Y'))
-        self.assertEqual('X', action_with_arg('X', v2='Y'))
-        self.assertEqual('X', action_with_kwargs(v='X', v2='Y'))
-
-    def test_multiple_values_for_same_argument(self):
-        with self.assertRaises(TypeError):
-            action_with_arg('X', v='Y')
-
-    def test_with_no_args(self):
-        with self.assertRaises(TypeError):
-            action_with_arg()
-        self.assertEqual('Y', action_with_kwargs())
-
-
 if __name__ == '__main__':
     unittest.main()
