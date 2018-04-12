c4e00ec81bf5ec1577552a259c9902d12cbad3c8
Joan Puigcerver <joapuipe@gmail.com>
2018-04-12 23:45:43 +0200

    minor comment

--- laia/decoders/ctc_greedy_decoder.py
+++ laia/decoders/ctc_greedy_decoder.py
@@ -25,8 +25,10 @@ class CTCGreedyDecoder(object):
         _, idx = x.max(dim=2)
         idx = idx.t().tolist()
         x = [idx_n[:int(xs[n])] for n, idx_n in enumerate(idx)]
+        # Remove repeated symbols
         x = [reduce(lambda z, x: z if z[-1] == x else z + [x],
                     x_n[1:], [x_n[0]]) for x_n in x]
+        # Remove CTC blank symbol
         self._output = [[x for x in x_n if x != 0] for x_n in x]
         return self._output
 
