1a80cd76d42c6d76a818c7e66e97b311f7f0b2e6
Joan Puigcerver <joapuipe@gmail.com>
2018-04-12 15:41:29 +0200

    Added option to monitor wer in HTR experiments

--- laia/engine/htr_engine_wrapper.py
+++ laia/engine/htr_engine_wrapper.py
@@ -6,16 +6,23 @@ from laia.hooks import action
 from laia.hooks.meters import RunningAverageMeter, SequenceErrorMeter, TimeMeter
 from laia.logging import get_logger
 from laia.losses import CTCLoss
+from laia.utils.char_to_word_seq import char_to_word_seq
 
 _logger = get_logger(__name__)
+
+
+def batch_char_to_word_seq(batch_sequence_of_characters, delimiters):
+    return ['_'.join(map(str, char_to_word_seq(seq_chars, delimiters)))
+            for seq_chars in batch_sequence_of_characters]
 
 
 class HtrEngineWrapper(object):
     """Engine wrapper to perform HTR experiments."""
 
-    def __init__(self, train_engine, valid_engine=None):
+    def __init__(self, train_engine, valid_engine=None, word_delimiters=None):
         self._tr_engine = train_engine
         self._va_engine = valid_engine
+        self._word_delimiters = word_delimiters
 
         # If the trainer was created without any criterion, or it is not
         # the CTCLoss, set it properly.
@@ -29,20 +36,19 @@ class HtrEngineWrapper(object):
         self._train_timer = TimeMeter()
         self._train_loss_meter = RunningAverageMeter()
         self._train_cer_meter = SequenceErrorMeter()
+        self._train_wer_meter = SequenceErrorMeter()
 
         self._tr_engine.add_hook(ON_EPOCH_START, self._train_reset_meters)
-        self._tr_engine.add_hook(ON_BATCH_END, self._train_accumulate_loss)
-        self._tr_engine.add_hook(ON_BATCH_END, self._train_compute_cer)
+        self._tr_engine.add_hook(ON_BATCH_END, self._train_update_meters)
 
         if valid_engine:
             self._valid_timer = TimeMeter()
             self._valid_loss_meter = RunningAverageMeter()
             self._valid_cer_meter = SequenceErrorMeter()
+            self._valid_wer_meter = SequenceErrorMeter()
 
             self._va_engine.add_hook(ON_EPOCH_START, self._valid_reset_meters)
-            self._va_engine.add_hook(ON_BATCH_END, self._valid_accumulate_loss)
-            self._va_engine.add_hook(ON_BATCH_END, self._valid_compute_cer)
-            self._va_engine.add_hook(ON_EPOCH_END, self._report_epoch_train_and_valid)
+            self._va_engine.add_hook(ON_BATCH_END, self._valid_update_meters)
 
             # Add evaluator to the trainer engine
             self._tr_engine.add_evaluator(self._va_engine)
@@ -50,7 +56,10 @@ class HtrEngineWrapper(object):
             self._valid_timer = None
             self._valid_loss_meter = None
             self._valid_cer_meter = None
-            self._tr_engine.add_hook(ON_EPOCH_END, self._report_epoch_train_only)
+
+        self._summary_format = None
+        self._summary_params = None
+        self._tr_engine.add_hook(ON_EPOCH_END, self._epoch_summary)
 
     @property
     def logger(self):
@@ -74,9 +83,20 @@ class HtrEngineWrapper(object):
     def valid_cer(self):
         return self._valid_cer_meter
 
+    def train_wer(self):
+        return self._train_wer_meter
+
+    def valid_wer(self):
+        return self._valid_cer_meter
+
     def run(self):
+        self._summary_format, self._summary_params = \
+            self._prepare_epoch_summary()
         self._tr_engine.run()
         return self
+
+    def set_word_delimiters(self, delimiters):
+        self._word_delimiters = delimiters
 
     @action
     def _train_reset_meters(self):
@@ -91,53 +111,73 @@ class HtrEngineWrapper(object):
         self._valid_cer_meter.reset()
 
     @action
-    def _train_accumulate_loss(self, batch_loss):
+    def _train_update_meters(self, batch_loss, batch_output, batch_target):
         self._train_loss_meter.add(batch_loss)
+        # Compute character error rate
+        batch_decode = self._ctc_decoder(batch_output)
+        self._train_cer_meter.add(batch_target, batch_decode)
+        # Compute word error rate, if word delimiters are given
+        if self._word_delimiters is not None:
+            decode_words = batch_char_to_word_seq(batch_decode,
+                                                  self._word_delimiters)
+            target_words = batch_char_to_word_seq(batch_target,
+                                                  self._word_delimiters)
+            self._train_wer_meter.add(target_words, decode_words)
         # Note: Stop training timer to avoid including extra costs
         # (e.g. the validation epoch)
         self._train_timer.stop()
 
     @action
-    def _valid_accumulate_loss(self, batch_output, batch_target):
+    def _valid_update_meters(self, batch_output, batch_target):
         batch_loss = self._tr_engine.criterion(batch_output, batch_target)
         self._valid_loss_meter.add(batch_loss)
+        # Compute character error rate
+        batch_decode = self._ctc_decoder(batch_output)
+        self._valid_cer_meter.add(batch_target, batch_decode)
+        # Compute word error rate, if word delimiters are given
+        if self._word_delimiters is not None:
+            decode_words = batch_char_to_word_seq(batch_decode,
+                                                  self._word_delimiters)
+            target_words = batch_char_to_word_seq(batch_target,
+                                                  self._word_delimiters)
+            self._valid_wer_meter.add(target_words, decode_words)
         # Note: Stop timer to avoid including extra costs
         self._valid_timer.stop()
 
-    @action
-    def _train_compute_cer(self, batch_output, batch_target):
-        batch_decode = self._ctc_decoder(batch_output)
-        self._train_cer_meter.add(batch_target, batch_decode)
+    def _prepare_epoch_summary(self):
+        fmt = 'Epoch {epoch:4d}'
+        # Note: We cannot add the epochs here, sine trainer.epochs is a method.
+        params = {'epoch': None}
+
+        fmt += ', TR Loss = {train_loss.value[0]:.3e}'
+        params['train_loss'] = self.train_loss()
+        if self._va_engine:
+            fmt += ', VA Loss = {valid_loss.value[0]:.3e}'
+            params['valid_loss'] = self.valid_loss()
+
+            fmt += ', TR CER = {train_cer.value:5.1%}'
+        params['train_cer'] = self.train_cer()
+        if self._va_engine:
+            fmt += ', VA CER = {valid_cer.value:5.1%}'
+            params['valid_cer'] = self.valid_cer()
+
+        if self._word_delimiters is not None:
+            fmt += ', TR WER = {train_wer.value:5.1%}'
+            params['train_wer'] = self.train_wer()
+            if self._va_engine:
+                fmt += ', VA CER = {valid_wer.value:5.1%}'
+                params['valid_wer'] = self.valid_cer()
+
+        fmt += ', TR Time = {train_timer.value:.2f}s'
+        params['train_timer'] = self.train_timer()
+        if self._va_engine:
+            fmt += ', VA Time = {valid_timer.value:.2f}s'
+            params['valid_timer'] = self.valid_timer()
+
+        return fmt, params
 
     @action
-    def _valid_compute_cer(self, batch_output, batch_target):
-        batch_decode = self._ctc_decoder(batch_output)
-        self._valid_cer_meter.add(batch_target, batch_decode)
-
-    @action
-    def _report_epoch_train_only(self):
-        self.logger.info('Epoch {epochs:4d}, '
-                         'TR Loss = {train_loss.value[0]:.3e}, '
-                         'TR CER = {train_cer.value:5.1%}, '
-                         'TR Time = {train_timer.value:.2f}s',
-                         epochs=self._tr_engine.epochs(),
-                         train_loss=self.train_loss(),
-                         train_cer=self.train_cer(),
-                         train_timer=self.train_timer())
-
-    @action
-    def _report_epoch_train_and_valid(self):
-        self.logger.info('Epoch {epochs:4d}, '
-                         'TR Loss = {train_loss.value[0]:.3e}, '
-                         'VA Loss = {valid_loss.value[0]:.3e}, '
-                         'TR CER = {train_cer.value:5.1%}, '
-                         'VA CER = {valid_cer.value:5.1%}, '
-                         'TR Time = {train_timer.value:.2f}s, '
-                         'VA Time = {valid_timer.value:.2f}s',
-                         epochs=self._tr_engine.epochs(),
-                         train_loss=self.train_loss(),
-                         valid_loss=self.valid_loss(),
-                         train_cer=self.train_cer(),
-                         valid_cer=self.valid_cer(),
-                         train_timer=self.train_timer(),
-                         valid_timer=self.valid_timer())
+    def _epoch_summary(self, epoch):
+        print(epoch)
+        self._summary_params['epoch'] = epoch
+        self.logger.info(self._summary_format, **self._summary_params)
