cb6eb74df05fcf648ee68fd96a814d349c2dcc33
Joan Puigcerver <joapuipe@gmail.com>
2018-04-12 23:44:48 +0200

    Working on washington experiment

--- egs/washington/launch_phocnet_train.sh
+++ egs/washington/launch_phocnet_train.sh
@@ -15,6 +15,7 @@ export PYTHONPATH=$HOME/src/PyLaia:$PYTHONPATH;
 TRAIN_TXT="data/lang/dortmund/char/${1}_tr.txt";
 VALID_TXT="data/lang/dortmund/char/${1}_te.txt";
 OUTPUT_DIR="$2";
+shift 2;
 
 for f in "$TRAIN_TXT" "$VALID_TXT"; do
   [ -s "$f" ] || { echo "File \"$f\" wasn't found!" >&2 && exit 1; }
@@ -22,12 +23,23 @@ done;
 
 mkdir -p "$OUTPUT_DIR";
 
-shift 2;
-python steps/train_phocnet.py \
+if [ -s "$OUTPUT_DIR/model.ckpt" ]; then
+    ckpt="$OUTPUT_DIR/model.ckpt";
+    msg="Checkpoint \"$ckpt\" already exists. Continue (c) or abort (a)? ";
+    read -p "$msg" -n 1 -r; echo;
+    if [[ $REPLY =~ ^[Cc]$ ]]; then
+       :
+    else
+        echo "Aborted training..." >&2;
+        exit 0;
+    fi;
+fi;
+
+python ./steps/train_phocnet.py \
        --max_epochs=220 \
-       --save_path="$OUTPUT_DIR" \
        --logging_also_to_stderr=INFO \
        --logging_file="$OUTPUT_DIR/train.log" \
+       --save_path="$OUTPUT_DIR" \
        $@ \
        train/dortmund/phoc_syms.txt \
        data/imgs/dortmund \
