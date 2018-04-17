25629e15dda12469f069908d179bb8c1dacdbca1
carmocca <carlossmocholi@gmail.com>
2018-04-17 20:17:44 +0200

    Add consecutive increasing/decreasing  conditions

--- laia/hooks/conditions/__init__.py
+++ laia/hooks/conditions/__init__.py
@@ -5,7 +5,7 @@ from laia.hooks.conditions.geq_than import GEqThan
 from laia.hooks.conditions.highest import Highest
 from laia.hooks.conditions.lowest import Lowest
 from laia.hooks.conditions.multiple_of import MultipleOf
-from laia.hooks.conditions.non_decreasing import NonDecreasing
-from laia.hooks.conditions.non_increasing import NonIncreasing
+from laia.hooks.conditions.non_decreasing import NonDecreasing, ConsecutiveNonDecreasing
+from laia.hooks.conditions.non_increasing import NonIncreasing, ConsecutiveNonIncreasing
 from laia.hooks.conditions.not_finite import NotFinite
 from laia.hooks.conditions.stddev_under import StdDevUnder
