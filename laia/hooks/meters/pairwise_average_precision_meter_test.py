29d381c4372841de24d1bc19cd6a58cc4b82b67e
jpuigcerver <joapuipe@gmail.com>
2018-05-01 14:36:38 +0200

    Added option to exclude words from mAP/gAP computation

--- laia/hooks/meters/pairwise_average_precision_meter_test.py
+++ laia/hooks/meters/pairwise_average_precision_meter_test.py
@@ -36,6 +36,17 @@ class AllPairsMetricAveragePrecisionMeterTest(unittest.TestCase):
         self.assertEqual(0.625, g_ap)
         self.assertEqual((1.0 + 1.0 / 3.0 + 1.0 + 1.0 / 3.0 + 0.0) / 5, m_ap)
 
+    def test_with_exclude_labels(self):
+        meter = PairwiseAveragePrecisionMeter(ignore_singleton=False,
+                                              exclude_labels=[3])
+        # Add batches to the meter
+        meter.add(*self.batch1)
+        meter.add(*self.batch2)
+
+        g_ap, m_ap = meter.value
+        self.assertEqual(8.0 / 12.0, g_ap)
+        self.assertEqual(8.0 / 12.0, m_ap)
+
 
 if __name__ == '__main__':
     unittest.main()
