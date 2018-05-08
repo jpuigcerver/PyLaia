b69ce7edf2777d566c7f710f773eabde5be38c56
Joan Puigcerver <joapuipe@gmail.com>
2018-05-08 04:09:31 +0200

    Updated CVL download.sh

--- egs/cvl/src/download.sh
+++ egs/cvl/src/download.sh
@@ -5,7 +5,14 @@ set -e;
 SDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
 cd "${SDIR}/..";
 
-mkdir -p data;
+mkdir -p data/images/words;
 
-wget -N -P data ftp://scruffy.cvl.tuwien.ac.at/staff/database/cvl-database/cvl-database-1-1.zip;
-[ -d data/cvl-database-1-1 ] || unzip -d data data/cvl-database-1-1.zip;
+NUM_PNG="$(find data/images/words -name "*.png" | wc -l)";
+[ "$NUM_PNG" -eq 99904 ] && exit 0;
+
+URL=ftp://scruffy.cvl.tuwien.ac.at/staff/database/cvl-database/cvl-database-1-1.zip;
+NUM_TIF="$(find data/cvl-database-1-1/ -name "*.tif" | wc -l)";
+[ "$NUM_TIF" -eq 114981 ] || {
+  wget -N -P data "$URL";
+  unzip -d data data/cvl-database-1-1.zip;
+}
