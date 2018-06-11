d75df7355ee342c83184c119ae1ef0e0dbbc45da
Joan Puigcerver <joapuipe@gmail.com>
2018-06-11 12:37:05 +0200

    Formatted using black.

--- laia/version.py
+++ laia/version.py
@@ -8,12 +8,12 @@ def git_commit():
     """
     Returns the hash of the current Git commit, or None if the package is not under Git.
     """
-    stderr = open(os.devnull, 'w')
+    stderr = open(os.devnull, "w")
     cwd = os.getcwd()
     commit = None
     try:
         os.chdir(os.path.dirname(__file__))
-        commit = subprocess.check_output(['git', 'rev-parse', 'HEAD'], stderr=stderr)
+        commit = subprocess.check_output(["git", "rev-parse", "HEAD"], stderr=stderr)
         commit = commit.strip()
     finally:
         os.chdir(cwd)
@@ -25,13 +25,14 @@ def git_branch():
     """
     Returns the name of the current Git branch, or None if the package is not under Git.
     """
-    stderr = open(os.devnull, 'w')
+    stderr = open(os.devnull, "w")
     cwd = os.getcwd()
     branch = None
     try:
         os.chdir(os.path.dirname(__file__))
         branch = subprocess.check_output(
-            ['git', 'rev-parse', '--abbrev-ref', 'HEAD'], stderr=stderr)
+            ["git", "rev-parse", "--abbrev-ref", "HEAD"], stderr=stderr
+        )
         branch = branch.strip()
     finally:
         os.chdir(cwd)
@@ -41,13 +42,14 @@ def git_branch():
 
 def git_describe():
     """Describes the current Git commit using the most recent tag."""
-    stderr = open(os.devnull, 'w')
+    stderr = open(os.devnull, "w")
     cwd = os.getcwd()
     describe = None
     try:
         os.chdir(os.path.dirname(__file__))
         describe = subprocess.check_output(
-            ['git', 'describe', '--tags', '--dirty', '--always'], stderr=stderr)
+            ["git", "describe", "--tags", "--dirty", "--always"], stderr=stderr
+        )
         describe = describe.strip()
     finally:
         os.chdir(cwd)
