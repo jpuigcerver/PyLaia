1844ddb4da2de03980e915728700fa0707392fb8
Joan Puigcerver <joapuipe@gmail.com>
2018-06-20 01:00:08 +0200

    Added tool to convert old PyLaia checkpoints to be loaded with the new VggRnnFixedHeight or LaiaCRNN modules

--- laia/models/htr/vgg_rnn_fixed_height.py
+++ laia/models/htr/vgg_rnn_fixed_height.py
@@ -1,18 +1,12 @@
 from itertools import count
 
+import re
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils.rnn import PackedSequence, pack_padded_sequence
 
 from laia.data import PaddedTensor
 from laia.models.htr.conv_block import ConvBlock
-
-
-def _get_height(x):
-    if isinstance(x, PaddedTensor):
-        return x.data.size(2)
-    else:
-        return x.size(2)
 
 
 class VggRnnFixedHeight(nn.Module):
@@ -40,7 +34,7 @@ class VggRnnFixedHeight(nn.Module):
         self._rnn_dropout = rnn_dropout
         self._lin_dropout = lin_dropout
         # Add convolutional blocks, in a VGG style.
-        self._conv_blocks = []
+        conv_blocks = []
         ni = num_input_channels
         for i, nh, ks, st, di, f, ps, dr, bn in zip(
             count(),
@@ -65,13 +59,22 @@ class VggRnnFixedHeight(nn.Module):
                 batchnorm=bn,
             )
             ni = nh
+            di_h = di[0] if isinstance(di, (list, tuple)) else di
+            ks_h = ks[0] if isinstance(ks, (list, tuple)) else ks
+            st_h = st[0] if isinstance(st, (list, tuple)) else st
             ps_h = ps[0] if isinstance(ps, (list, tuple)) else ps
-            ps_h = ps_h if ps_h > 1 else 1
-            input_height = input_height // ps_h
-            self.add_module("conv_block{}".format(i), layer)
-            self._conv_blocks.append(layer)
+            ps_h = ps_h if ps_h > 1 else None
+            input_height = ConvBlock.get_output_size(
+                size=input_height,
+                dilation=di_h,
+                kernel_size=ks_h,
+                stride=st_h,
+                poolsize=ps_h,
+            )
+            conv_blocks.append(layer)
+        self.conv = Sequential(*conv_block)
 
-        rnn = rnn_type(
+        self.rnn = rnn_type(
             input_height * ni,
             rnn_units,
             rnn_layers,
@@ -79,38 +82,27 @@ class VggRnnFixedHeight(nn.Module):
             bidirectional=True,
             batch_first=False,
         )
-        self.add_module("rnn", rnn)
-        self._rnn = rnn
+        self.rnn.flatten_parameters()
 
-        linear = nn.Linear(2 * rnn_units, num_output_labels)
-        self.add_module("linear", linear)
-        self._linear = linear
+        self.linear = nn.Linear(2 * rnn_units, num_output_labels)
 
     def forward(self, x):
-        assert _get_height(x) == self._input_height, (
-            "Input image height ({}) is not the "
-            "expected ({})".format(_get_height(x), self._input_height)
-        )
-        is_padded = isinstance(x, PaddedTensor)
-        for block in self._conv_blocks:
-            x = block(x)
-        if is_padded:
-            x, xs = x.data, x.sizes
+        x = self.conv(x)
+        x, xs = (x.data, x.sizes) if isinstance(x, PaddedTensor) else (x, None)
         # Note: x shape is N x C x H x W -> W x N x (H * C)
         N, C, H, W = x.size()
         x = x.permute(3, 0, 1, 2).contiguous().view(W, N, H * C)
         if self._rnn_dropout:
             x = F.dropout(x, self._rnn_dropout, training=self.training)
-        if is_padded:
+        if xs:
             x = pack_padded_sequence(x, list(xs.data[:, 1]))
-        self._rnn.flatten_parameters()
-        x, _ = self._rnn(x)
+        x, _ = self.rnn(x)
         # Output linear layer
-        if is_padded:
+        if xs:
             x, xs = x.data, x.batch_sizes
         if self._lin_dropout:
             x = F.dropout(x, self._lin_dropout, training=self.training)
-        x = self._linear(x)
-        if is_padded:
+        x = self.linear(x)
+        if xs:
             x = PackedSequence(x, batch_sizes=xs)
         return x
