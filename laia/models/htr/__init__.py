7890e4cdaa03dbc844a9ce89d3663abc298626f3
carmocca <carlossmocholi@gmail.com>
2018-06-21 18:41:23 +0200

    Remove old model
    See `laia.utils.convert_old_model_checkpoint` to reuse old trained models parameters

--- laia/models/htr/__init__.py
+++ laia/models/htr/__init__.py
@@ -3,5 +3,4 @@ from __future__ import absolute_import
 from laia.models.htr.conv_block import ConvBlock
 from laia.models.htr.dummy_model import DummyModel
 from laia.models.htr.laia_crnn import LaiaCRNN
-from laia.models.htr.vgg_rnn_fixed_height import VggRnnFixedHeight
 from laia.models.htr.gated_crnn import GatedConv2d, GatedCRNN
