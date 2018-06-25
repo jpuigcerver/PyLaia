48d0e82db5cd5662b227555bafdd3bad968b6048
jpuigcerver <joapuipe@gmail.com>
2018-06-25 23:25:18 +0200

    Refactor test examples

---
+++ egs/test/fixed_height_single_image_batch_size_5/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e;
+
+SDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
+cd "$SDIR";
+export PYTHONPATH="$PWD/../../..:$PYTHONPATH";
+
+train_path="$PWD";
+rm -f *.log *.ckpt* trainer model;
+
+../../../pylaia-htr-create-model \
+  --cnn_num_features 16 32 \
+  --cnn_kernel_size 3 3 \
+  --cnn_stride 1 1 \
+  --cnn_dilation 1 1 \
+  --cnn_poolsize 2 2 \
+  --cnn_dropout 0 0 \
+  --cnn_activations ReLU ReLU \
+  --cnn_batchnorm false false \
+  --rnn_units 64 \
+  --rnn_dropout 0 \
+  --lin_dropout 0 \
+  --train_path="$train_path" \
+  -- 78 1 syms.txt;
+
+../../../pylaia-htr-train-ctc \
+  --logging_also_to_stderr info \
+  --logging_file train.log \
+  --logging_level info \
+  --batch_size 5 \
+  --learning_rate 0.001 \
+  --gpu 0 \
+  --max_epochs 500 \
+  --train_path="$train_path" \
+  -- syms.txt ../imgs_h78 gt.txt gt.txt
---
+++ egs/test/plot_log.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e;
+
+[ $# -lt 2 ] && echo "Usage: ${0##*/} measure log [log ...]" >&2 && exit 1;
+case "$1" in
+  tr_loss)
+    regex="^.* Epoch +([0-9]+),.* TR Loss = ([0-9e+.-]+), .*$";
+  ;;
+  va_loss)
+    regex="^.* Epoch +([0-9]+),.* VA Loss = ([0-9e+.-]+), .*$";
+  ;;
+  *)
+    echo "Unknown measure \"$1\"!" >&2;
+    exit 1;
+esac;
+shift 1;
+
+files=();
+tmpfiles=();
+while [ $# -gt 0 ]; do
+  files+=("$(echo "$1" | sed -r 's|_|\\_|g')");
+  tmpfiles+=("$(mktemp)");
+  gawk -v regex="$regex" 'BEGIN{
+    print(regex);
+  }{
+    if(match($0, regex, arr)) {
+      print arr[1], arr[2];
+    }
+  }' "$1" > "${tmpfiles[-1]}";
+  shift 1;
+done;
+
+{
+  echo "plot \\";
+  for i in $(seq "${#tmpfiles[@]}"); do
+    echo -n "'${tmpfiles[i-1]}' u 1:2 w l t '${files[i-1]}'";
+    if [ "$i" -lt "${#tmpfiles[@]}" ]; then
+      echo ", \\";
+    else
+      echo "";
+    fi;
+  done;
+} | gnuplot -p;
+rm -f "${#tmpfiles[@]}";
