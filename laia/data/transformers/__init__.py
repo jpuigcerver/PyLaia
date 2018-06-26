cc3b426b48926f990a3a2bdb6a53b51f3fd0564a
Joan Puigcerver <joapuipe@gmail.com>
2018-06-26 20:11:53 +0200

    Added basic transformers. Refactored Action and Hook names

---
+++ laia/data/transformers/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+from laia.data.transformers.transformer import Transformer
+from laia.data.transformers.transformer import TransformerChoice
+from laia.data.transformers.transformer import TransformerConditional
+from laia.data.transformers.transformer import TransformerList
--- laia/distorter/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-try:
-    from laia.distorter.image_distorter import ImageDistorter
-except ImportError:
-    # TODO: Show a warning.
-    pass
