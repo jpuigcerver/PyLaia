e7561c92134b35c4aae58b2c8add3f351900ab91
jpuigcerver <joapuipe@gmail.com>
2018-07-04 12:49:03 +0200

    Working on icdar2015kws

--- egs/icdar2015kws/src/generate_lattices_char_lm.sh
+++ egs/icdar2015kws/src/generate_lattices_char_lm.sh
@@ -13,27 +13,34 @@ ngram_order=7;
 prior_scale=0.3;
 query_nbest=100;
 help_message="
-Usage: ${0##*/} [Options] <lkhs_dir> <output_dir>
+Usage: ${0##*/} [Options] <lkhs_dir> <graph_dir> <lats_dir>
+
+Options:
+  --beam         : (type = float, default = $beam)
+  --lattice_beam : (type = float, default = $lattice_beam)
+  --ngram_order  : (type = integer, default = $ngram_order)
+  --prior_scale  : (type = float, default = $prior_scale)
+  --query_nbest  : (type = integer, default = $query_nbest)
 ";
 source "$PWD/../utils/parse_options.inc.sh" || exit 1;
-[ $# -ne 2 ] && echo "$help_message" >&2 && exit 1;
+[ $# -ne 3 ] && echo "$help_message" >&2 && exit 1;
 
 lkhs_dir="$1";
-output_dir="$2";
+graph_dir="$2";
+lats_dir="$3";
 check_all_files -s "$lkhs_dir/va_ps${prior_scale}.mat.ark";
 
 
 ###########################################################
-## 1. Build FSTs needed for generating the lattices
+## 1. Build FSTs needed for generating the line lattices
 ###########################################################
 ./src/prepare_char_lm_fsts.sh \
-  --ngram_order "$ngram_order" "$output_dir" || exit 1;
+  --ngram_order "$ngram_order" "$graph_dir" || exit 1;
 
 
 ###########################################################
 ## 2. Generate and align character validation lattices
 ###########################################################
-lats_dir="${output_dir}/lats/ps${prior_scale}_b${beam}_lb${lattice_beam}";
 mkdir -p "$lats_dir";
 lkh="$lkhs_dir/va_ps${prior_scale}.mat.ark";
 lat="${lats_dir}/va.lat";
@@ -44,12 +51,12 @@ latgen-faster-mapped-parallel \
   --lattice-beam="$lattice_beam" \
   --num-threads=$(nproc) \
   --prune-interval=500 \
-  "$output_dir/model" \
-  "$output_dir/HCLG.fst" \
+  "$graph_dir/model" \
+  "$graph_dir/HCLG.fst" \
   "ark:$lkh" ark:- |
 lattice-align-words-lexicon \
-  "$output_dir/lexicon_align.txt" \
-  "$output_dir/model" \
+  "$graph_dir/lexicon_align.txt" \
+  "$graph_dir/model" \
   ark:- "ark,scp:$lat.ark,$lat.scp";
 ) 2>&1 | tee "${lats_dir}/va.log" >&2;
 
