64f405ee1066bf885d0b99e68ef08b5f20d78aee
Joan Puigcerver <joapuipe@gmail.com>
2018-08-11 14:43:19 +0200

    Refactoring transformers

--- laia/data/transformers/transformer_image_invert.py
+++ laia/data/transformers/transformer_image_invert.py
@@ -9,14 +9,10 @@ from laia.data.transformers.transformer import Transformer
 class TransformerImageInvert(Transformer):
     """Invert the colors of a PIL image with the given probability."""
 
-    def __init__(self, probability=0.5):
+    def __init__(self):
         # type: (float) -> None
         super(TransformerImageInvert, self).__init__()
-        self.probability = probability
 
     def __call__(self, x):
         # type: (Image) -> Image
-        if np.random.random() < self.probability:
-            return ImageOps.invert(x)
-        else:
-            return x
+        return ImageOps.invert(x)
