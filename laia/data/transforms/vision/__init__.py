68b3ddd25dcc1dd2d195100deffdbbdaa1556f69
carmocca <carlossmocholi@gmail.com>
2018-08-20 15:49:38 +0200

    Add 'Beta' prefix to Morphology and Perspective transforms

--- laia/data/transforms/vision/__init__.py
+++ laia/data/transforms/vision/__init__.py
@@ -1,4 +1,4 @@
 from laia.data.transforms.vision.random_beta_affine import RandomBetaAffine
-from laia.data.transforms.vision.random_morphology import Dilate, Erode
-from laia.data.transforms.vision.random_perspective import RandomPerspective
+from laia.data.transforms.vision.random_beta_morphology import Dilate, Erode
+from laia.data.transforms.vision.random_beta_perspective import RandomBetaPerspective
 from laia.data.transforms.vision.transforms import *
