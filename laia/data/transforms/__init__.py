966b46086c8fd1d490fae390082b8d6a173d45c5
carmocca <carlossmocholi@gmail.com>
2018-08-20 13:31:29 +0200

    Separate transforms into vision and base packages

--- laia/data/transforms/__init__.py
+++ laia/data/transforms/__init__.py
@@ -1,6 +1,4 @@
 from __future__ import absolute_import
 
-from laia.data.transforms.random_beta_affine import RandomBetaAffine
-from laia.data.transforms.random_morphology import Dilate, Erode
-from laia.data.transforms.random_perspective import RandomPerspective
-from laia.data.transforms.transforms import RandomProbChoice, Invert, Convert
+from laia.data.transforms.transforms import *
+from laia.data.transforms import vision
