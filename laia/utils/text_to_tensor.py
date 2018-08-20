ffb5996ee43e4b0d4a381743f9fb039943a48edd
carmocca <carlossmocholi@gmail.com>
2018-08-20 14:45:08 +0200

    Add deprecation warning

--- laia/utils/text_to_tensor.py
+++ laia/utils/text_to_tensor.py
@@ -1,4 +1,13 @@
-from laia.data.transforms.text.transforms import ToTensor
+import warnings
+
+from laia.data.transforms.text import ToTensor
+
 
 # TODO: Remove this
-TextToTensor = ToTensor
+class TextToTensor(ToTensor):
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            "The use of laia.utils.TextToTensor is deprecated, "
+            "please use laia.data.transforms.text.ToTensor instead."
+        )
+        super(TextToTensor, self).__init__(*args, **kwargs)
