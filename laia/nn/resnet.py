8be2b71d7c9ca43822127596d85218f277f00ffa
jpuigcerver <joapuipe@gmail.com>
2019-09-08 17:51:23 +0200

    Update travis yml

--- laia/nn/resnet.py
+++ laia/nn/resnet.py
@@ -1,48 +1,316 @@
-from __future__ import absolute_import
+from typing import Optional, Sequence, Union
 
+import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-from laia.data import PaddedTensor
+from laia.data.padding_collater import PaddedTensor
 
 
-class ResnetConv2dBlock(nn.Module):
-    def __init__(self, in_planes, out_planes, stride=1, batch_norm=True):
-        super(ResnetConv2dBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False
+def conv3x3(in_planes, out_planes, stride=1, groups=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(
+        in_planes,
+        out_planes,
+        kernel_size=3,
+        stride=stride,
+        padding=1,
+        groups=groups,
+        bias=False,
+    )
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+def downsample(inplanes, planes, expansion, stride=1, norm_layer=None):
+    if stride != 1 or inplanes != planes * expansion:
+        downsample = [conv1x1(inplanes, planes * expansion, stride)]
+        if norm_layer is not None:
+            downsample.append(norm_layer(planes * expansion))
+        return nn.Sequential(*downsample)
+    else:
+        return None
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, groups=1, norm_layer=None):
+        super(BasicBlock, self).__init__()
+        if groups != 1:
+            raise ValueError("BasicBlock only supports groups=1")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = None if norm_layer is None else norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = None if norm_layer is None else norm_layer(planes)
+        self.downsample = downsample(
+            inplanes, planes, self.expansion, stride, norm_layer
         )
-        self.conv2 = nn.Conv2d(
-            out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False
-        )
-        if batch_norm:
-            self.bn1 = nn.BatchNorm2d(out_planes)
-            self.bn2 = nn.BatchNorm2d(out_planes)
-        else:
-            self.bn1 = lambda x: x
-            self.bn2 = lambda x: x
-
         self.stride = stride
-        if stride != 1 or in_planes != out_planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(
-                    in_planes, out_planes, kernel_size=1, stride=stride, bias=False
-                ),
-                nn.BatchNorm2d(out_planes),
-            )
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        if self.bn1 is not None:
+            out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        if self.bn2 is not None:
+            out = self.bn2(out)
+
+        if self.downsample:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, groups=1, norm_layer=None):
+        super(Bottleneck, self).__init__()
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, planes)
+        self.bn1 = None if norm_layer is None else norm_layer(planes)
+        self.conv2 = conv3x3(planes, planes, stride, groups)
+        self.bn2 = None if norm_layer is None else norm_layer(planes)
+        self.conv3 = conv1x1(planes, planes * self.expansion)
+        self.bn3 = None if norm_layer is None else norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample(
+            inplanes, planes, self.expansion, stride, norm_layer
+        )
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        if self.bn1 is not None:
+            out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        if self.bn2 is not None:
+            out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        if self.bn3 is not None:
+            out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResnetOptions(object):
+    def __init__(
+        self,
+        block: Union[BasicBlock, Bottleneck],
+        input_channels: int = 1,
+        root_kernel: int = 5,
+        layers: Sequence[int] = (2, 2, 2, 2),
+        stride: Sequence[int] = (1, 2, 1, 1),
+        zero_init_residual: bool = False,
+        groups: int = 1,
+        width_per_group: int = 64,
+        norm_layer: Optional[nn.Module] = None,
+    ):
+
+        if len(layers) != 4:
+            raise ValueError("The length of layers should be 4")
+
+        if len(stride) != 4:
+            raise ValueError("The length of stride should be 4")
+
+        if root_kernel not in [3, 5, 7]:
+            raise ValueError("The root_kernel must be 3, 5 or 7")
+
+        self._input_channels = input_channels
+        self._root_kernel = root_kernel
+        self._root_padding = {3: 1, 5: 2, 7: 3}[root_kernel]
+        self._block = block
+        self._layers = layers
+        self._stride = stride
+        self._zero_init_residual = zero_init_residual
+        self._groups = groups
+        self._width_per_group = width_per_group
+        self._norm_layer = norm_layer
+
+        self._planes = [int(width_per_group * groups * 2 ** i) for i in range(4)]
+
+    @property
+    def input_channels(self):
+        return self._input_channels
+
+    @property
+    def root_kernel(self):
+        return self._root_kernel
+
+    @property
+    def root_padding(self):
+        return self._root_padding
+
+    @property
+    def block(self):
+        return self._block
+
+    @property
+    def layers(self):
+        return self._layers
+
+    @property
+    def stride(self):
+        return self._stride
+
+    @property
+    def zero_init_residual(self):
+        return self._zero_init_residual
+
+    @property
+    def groups(self):
+        return self._groups
+
+    @property
+    def width_per_group(self):
+        return self._width_per_group
+
+    @property
+    def norm_layer(self):
+        return self._norm_layer
+
+    @property
+    def planes(self):
+        return self._planes
+
+
+class ResnetConv(nn.Module):
+    def __init__(self, options: ResnetOptions):
+        super(ResnetConv, self).__init__()
+
+        self.inplanes = options.planes[0]
+        self.conv1 = nn.Conv2d(
+            in_channels=options.input_channels,
+            out_channels=options.planes[0],
+            kernel_size=options.root_kernel,
+            stride=2,
+            padding=options.root_padding,
+            bias=False,
+        )
+        if options.norm_layer is None:
+            self.bn1 = None
         else:
-            self.shortcut = lambda x: x
+            self.bn1 = options.norm_layer(options.planes[0])
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        for i in range(4):
+            setattr(
+                self,
+                "layer%d" % (i + 1),
+                self._make_layer(
+                    block=options.block,
+                    planes=options.planes[i],
+                    blocks=options.layers[i],
+                    stride=options.stride[i],
+                    groups=options.groups,
+                    norm_layer=options.norm_layer,
+                ),
+            )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if options.zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+        self._options = options
+
+    @property
+    def options(self):
+        return self._options
+
+    def _make_layer(self, block, planes, blocks, stride, groups, norm_layer):
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, groups, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(
+                block(self.inplanes, planes, groups=groups, norm_layer=norm_layer)
+            )
+
+        return nn.Sequential(*layers)
 
     def forward(self, x):
         if isinstance(x, PaddedTensor):
             x, xs = x.data, x.sizes
+            assert xs.dim() == 2, "PaddedTensor.sizes must be a matrix"
+            assert xs.size(1) == 2, (
+                "PaddedTensor.sizes must have 2 columns: Height and Width, "
+                "{} columns given instead.".format(xs.size(1))
+            )
+            assert x.size(0) == xs.size(0), (
+                "Number of batch sizes ({}) does not match the number of "
+                "samples in the batch {}".format(xs.size(0), x.size(0))
+            )
         else:
             xs = None
-        y = F.relu(self.bn1(self.conv1(x)))
-        y = self.bn2(self.conv2(y))
-        y += self.shortcut(x)
-        y = F.relu(y)
-        if xs is not None:
-            ys = (xs + self.stride - 1) / self.stride
-            y = PaddedTensor(y, ys)
-        return y
+        assert x.size(1) == self._options.input_channels, (
+            "Input image depth ({}) does not match the "
+            "expected ({})".format(x.size(1), self._options.input_channels)
+        )
+
+        x = self.conv1(x)
+        if self.bn1 is not None:
+            x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        if xs is None:
+            return x
+        else:
+            return PaddedTensor(x, self.get_output_batch_size(xs))
+
+    def get_output_batch_size(self, xs):
+        return self.get_output_size(xs, self._options)
+
+    @staticmethod
+    def get_output_size(
+        size: Union[torch.Tensor, int], options: ResnetOptions
+    ) -> Union[torch.Tensor, int]:
+        # (size + 2 * padding - dilation * (kernel - 1) + stride - 1)  // stride
+        size = (size + 1) // 2  # After self.conv1
+        size = (size + 1) // 2  # After self.maxpool
+        for i in range(4):  # After layer i
+            size = (size + options.stride[i] - 1) // options.stride[i]
+        return size
