9b66d8920825349fa1b95cd76687a2f1900db452
Carlos Mocholí <carlossmocholi@gmail.com>
2020-07-10 19:08:08 +0200

    Remove PHOC/Dortmund code

--- laia/utils/__init__.py
+++ laia/utils/__init__.py
@@ -1,6 +1,4 @@
 from laia.utils.checks import check_inf, check_nan
 from laia.utils.image_collage import image_collage
-from laia.utils.image_to_tensor import ImageToTensor
 from laia.utils.kaldi import ArchiveLatticeWriter, ArchiveMatrixWriter
 from laia.utils.symbols_table import SymbolsTable
-from laia.utils.text_to_tensor import TextToTensor
