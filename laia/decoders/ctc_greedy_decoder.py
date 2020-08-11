eb4914f6884833367c8ea0f5d6b2e6ca631a7ea7
Carlos Mocholí <carlossmocholi@gmail.com>
2020-08-11 15:01:59 +0200

    Add char/word segmentation to decode

--- laia/decoders/ctc_greedy_decoder.py
+++ laia/decoders/ctc_greedy_decoder.py
@@ -1,4 +1,4 @@
-from functools import reduce
+from itertools import groupby
 
 from laia.losses.ctc_loss import transform_output
 
@@ -6,21 +6,30 @@ from laia.losses.ctc_loss import transform_output
 class CTCGreedyDecoder:
     def __init__(self):
         self._output = None
+        self._segmentation = None
 
-    def __call__(self, x):
+    def __call__(self, x, segmentation=False):
         x, xs = transform_output(x)
-        _, idx = x.max(dim=2)
-        idx = idx.t().tolist()
-        x = [idx_n[: int(xs[n])] for n, idx_n in enumerate(idx)]
+        idx = x.argmax(dim=2).t().tolist()
+        x = [idx_n[: xs[n].item()] for n, idx_n in enumerate(idx)]
+        if segmentation:
+            self._segmentation = [
+                [n for n, v in enumerate(x_n) if n == 0 or v != x_n[n - 1] != 0]
+                + [len(x_n) - 1]
+                for x_n in x
+            ]
         # Remove repeated symbols
-        x = [
-            reduce(lambda z, x: z if z[-1] == x else z + [x], x_n[1:], [x_n[0]])
-            for x_n in x
-        ]
+        x = [[k for k, _ in groupby(x_n)] for x_n in x]
         # Remove CTC blank symbol
         self._output = [[x for x in x_n if x != 0] for x_n in x]
+        if segmentation:
+            assert any(len(self._segmentation) == len(self._output) + n for n in (1, 2))
         return self._output
 
     @property
     def output(self):
         return self._output
+
+    @property
+    def segmentation(self):
+        return self._segmentation
