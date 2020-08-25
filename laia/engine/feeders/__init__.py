042ae306fa9470b3bfc193e0229f46f885d52cbc
Carlos Mocholí <carlossmocholi@gmail.com>
2020-08-25 23:23:09 +0200

    Refactor feeders
    IMPORTANT: pylaia-htr* scripts have not been updated

--- laia/engine/feeders/__init__.py
+++ laia/engine/feeders/__init__.py
@@ -1,3 +1,1 @@
-from laia.engine.feeders.feeder import Feeder, ItemFeeder
-from laia.engine.feeders.image_feeder import ImageFeeder
-from laia.engine.feeders.tensor_feeder import TensorFeeder
+from laia.engine.feeders.feeder import *
