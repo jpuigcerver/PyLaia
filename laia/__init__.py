ac7c9e824d1177dcb137a82301e7e44fb4ac7196
Carlos Mocholí <carlossmocholi@gmail.com>
2020-08-25 23:34:39 +0200

    Add get_installed_versions()

--- laia/__init__.py
+++ laia/__init__.py
@@ -13,7 +13,10 @@ __all__ = [
     "nn",
     "utils",
     "__version__",
+    "get_installed_versions",
 ]
+
+from typing import List
 
 import laia.common
 import laia.conditions
@@ -39,3 +42,17 @@ except ImportError:
             v.node, v.branch, ".dirty" if v.dirty else ""
         ),
     )
+
+
+def get_installed_versions() -> List[str]:
+    import subprocess
+
+    with open("requirements.txt") as f:
+        requirements = [r.strip() for r in f.readlines()]
+    freeze = subprocess.check_output(["pip", "freeze", "--exclude-editable"])
+    freeze = freeze.decode("ascii").strip().split()
+    versions = [
+        r for r in freeze if r in requirements or r[: r.index("==")] in requirements
+    ]
+    versions.append(f"laia=={__version__}")
+    return versions
