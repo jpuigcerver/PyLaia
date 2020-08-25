fb43c1e611b017fb56848fb3e2ff577e9b8f5725
Carlos Mocholí <carlossmocholi@gmail.com>
2020-08-25 23:19:07 +0200

    Add random_transform to vision.transforms.ToImageTensor
    Add __repr__

--- laia/data/transforms/vision/transforms.py
+++ laia/data/transforms/vision/transforms.py
@@ -1,3 +1,6 @@
+from typing import Optional, Callable
+
+import torch
 import torchvision
 from PIL import ImageOps, Image
 
@@ -33,56 +36,78 @@ class Convert:
 class ToImageTensor:
     def __init__(
         self,
-        invert=True,
-        mode="L",
-        fixed_height=None,
-        fixed_width=None,
-        min_height=None,
-        min_width=None,
-        pad_color=0,
-    ):
+        invert: bool = True,
+        mode: str = "L",
+        random_transform: Optional[Callable] = None,
+        fixed_height: Optional[int] = None,
+        fixed_width: Optional[int] = None,
+        min_height: Optional[int] = None,
+        min_width: Optional[int] = None,
+        pad_color: int = 0,
+    ) -> None:
         assert mode in ("L", "RGB", "RGBA")
         assert fixed_height is None or fixed_height > 0
         assert fixed_width is None or fixed_width > 0
         assert min_height is None or min_height > 0
         assert min_width is None or min_width > 0
         self._convert_transform = Convert(mode)
-        self._invert_transform = Invert() if invert else lambda x: x
+        self._should_invert = invert
+        self._invert_transform = Invert() if self._should_invert else lambda x: x
+        self._random_transform = (
+            lambda x: random_transform(x) if random_transform else x
+        )
+        self._should_resize = fixed_height or fixed_width
         self._resize_transform = (
             lambda x: self.resize_transform(x, fixed_height, fixed_width)
-            if fixed_height or fixed_width
+            if self._should_resize
             else x
         )
+        self._should_pad = min_height or min_width
         self._pad_transform = (
             lambda x: self.pad_transform(x, min_height, min_width, pad_color=pad_color)
-            if min_height or min_width
+            if self._should_pad
             else x
         )
         self._tensor_transform = ToTensor()
 
-    def __call__(self, img):
+    def __call__(self, img: Image.Image) -> torch.Tensor:
         assert isinstance(img, Image.Image)
         img = self._convert_transform(img)
         img = self._invert_transform(img)
+        img = self._random_transform(img)
         img = self._resize_transform(img)
         img = self._pad_transform(img)
         img = self._tensor_transform(img)
+        # C x H x W
         return img
 
     @staticmethod
-    def resize_transform(img, fh, fw):
+    def resize_transform(
+        img: Image.Image, fh: Optional[int], fw: Optional[int]
+    ) -> Image.Image:
         w, h = img.size
         nh = fh if fh else h * fw // w
         nw = fw if fw else w * fh // h
         return img.resize((nw, nh), Image.BILINEAR)
 
     @staticmethod
-    def pad_transform(img, mh, mw, pad_color=0):
+    def pad_transform(
+        img: Image.Image, mh: Optional[int], mw: Optional[int], pad_color: int = 0
+    ) -> Image.Image:
         w, h = img.size
         nh = h if mh is None or h >= mh else mh
         nw = w if mw is None or w >= mw else mw
         return torchvision.transforms.functional.pad(
             img, ((nw - w) // 2, (nh - h) // 2), fill=pad_color
+        )
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__ + (
+            f"(\n  {self._convert_transform},\n  "
+            + (f"{self._invert_transform},\n  " if self._should_invert else "")
+            + ("vision.resize_transform(),\n  " if self._should_resize else "")
+            + ("vision.pad_transform(),\n  " if self._should_pad else "")
+            + f"{self._tensor_transform}\n)"
         )
 
 
