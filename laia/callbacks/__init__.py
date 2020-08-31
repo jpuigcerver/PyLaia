cb2c551021090c2d7ffda492464e1a0edaab64c2
Carlos Mocholí <carlossmocholi@gmail.com>
2020-08-31 23:30:19 +0200

    htr-netout working

--- laia/callbacks/__init__.py
+++ laia/callbacks/__init__.py
@@ -1,4 +1,5 @@
 from laia.callbacks.decode import Decode
 from laia.callbacks.learning_rate import LearningRate
+from laia.callbacks.netout import Netout
 from laia.callbacks.progress_bar import ProgressBar
 from laia.callbacks.training_timer import TrainingTimer
