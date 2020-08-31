242e84d660e212b2a44fd21872cf92d5cb4cebc4
Carlos Mocholí <carlossmocholi@gmail.com>
2020-08-31 02:26:46 +0200

    Update setup.py. Add MANIFEST

--- setup.py
+++ setup.py
@@ -2,23 +2,18 @@ import os
 
 import setuptools
 
-
-def get_scripts():
-    return [
-        os.path.join(os.path.dirname(os.path.abspath(__file__)), script)
-        for script in (
-            "pylaia-htr-create-model",
-            "pylaia-htr-decode-ctc",
-            "pylaia-htr-train-ctc",
-            "pylaia-htr-netout",
-        )
-    ]
+DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_requirements():
-    requirements_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
-    with open(requirements_file, "r", encoding="utf-8") as f:
+    requirements_path = os.path.join(DIR, "requirements.txt")
+    with open(requirements_path, encoding="utf-8") as f:
         return [line.strip() for line in f]
+
+
+def get_long_description():
+    readme_path = os.path.join(DIR, "README.md")
+    return open(readme_path, encoding="utf-8").read()
 
 
 setuptools.setup(
@@ -30,8 +25,11 @@ setuptools.setup(
     },
     author="Joan Puigcerver",
     author_email="joapuipe@gmail.com",
+    maintainer="Carlos Mocholí",
+    maintainer_email="carlossmocholi@gmail.com",
     license="MIT",
     url="https://github.com/jpuigcerver/PyLaia",
+    download_url="https://github.com/jpuigcerver/PyLaia",
     # Requirements
     setup_requires=["setuptools_scm"],
     install_requires=get_requirements(),
@@ -42,5 +40,13 @@ setuptools.setup(
     python_requires=">=3.6",
     # Package contents
     packages=setuptools.find_packages(exclude=["tests"]),
-    scripts=get_scripts(),
+    include_package_data=True,
+    scripts=[
+        "pylaia-htr-create-model",
+        "pylaia-htr-train-ctc",
+        "pylaia-htr-decode-ctc",
+        "pylaia-htr-netout",
+    ],
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
 )
