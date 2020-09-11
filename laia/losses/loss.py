c5933cd3990d996c7a550b1fe198f53deed2b093
Carlos Mocholí <carlossmocholi@gmail.com>
2020-09-11 05:15:26 +0200

    Minor fixes

--- laia/losses/loss.py
+++ laia/losses/loss.py
@@ -5,5 +5,5 @@ class Loss(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, outputs, targets, **kwargs):
+    def forward(self, output, target, **kwargs):
         raise NotImplementedError
