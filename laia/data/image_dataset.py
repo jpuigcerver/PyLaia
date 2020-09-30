07c8cb78aff948b0a0be65129c6ad7788cf5cb3c
Carlos Mocholí <carlossmocholi@gmail.com>
2020-09-30 04:10:50 +0200

    Add ImageFromListDataset tests. Code improvements

--- laia/data/image_dataset.py
+++ laia/data/image_dataset.py
@@ -1,15 +1,19 @@
+from typing import Any, Callable, Dict, List, Optional
+
 import torch
 from PIL import Image
 
 
 class ImageDataset(torch.utils.data.Dataset):
-    def __init__(self, imgs, transform=None):
+    def __init__(
+        self, imgs: List[str], transform: Optional[Callable[[Image.Image], Any]] = None
+    ):
         assert isinstance(imgs, (list, tuple))
         super().__init__()
         self._imgs = imgs
         self._transform = transform
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> Dict[str, Any]:
         """Returns a dictionary containing the given image from the dataset.
         The image is associated with the key 'img'."""
         img = Image.open(self._imgs[index])
@@ -17,5 +21,5 @@ class ImageDataset(torch.utils.data.Dataset):
             img = self._transform(img)
         return {"img": img}
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._imgs)
