a074cc86ce23a3f566b6cfdf550f7f9e5e8087fe
Carlos Mocholí <carlossmocholi@gmail.com>
2020-10-02 00:25:11 +0200

    Warn about missing symbol

--- laia/data/transforms/text/transforms.py
+++ laia/data/transforms/text/transforms.py
@@ -1,6 +1,9 @@
 from typing import Dict, List, Union
 
+import laia.common.logging as log
 from laia.utils.symbols_table import SymbolsTable
+
+_logger = log.get_logger(__name__)
 
 
 class ToTensor:
@@ -9,10 +12,17 @@ class ToTensor:
         self._syms = syms
 
     def __call__(self, x: str) -> List[int]:
-        return [
-            self._syms.get(c, None) if isinstance(self._syms, Dict) else self._syms[c]
-            for c in x.split()
-        ]
+        values = []
+        for c in x.split():
+            v = (
+                self._syms.get(c, None)
+                if isinstance(self._syms, Dict)
+                else self._syms[c]
+            )
+            if v is None:
+                _logger.error('Could not find "{}" in the symbols table', c)
+            values.append(v)
+        return values
 
     def __repr__(self) -> str:
         return f"text.{self.__class__.__name__}()"
--- tests/data/transforms/text/text_transforms_test.py
+++ tests/data/transforms/text/text_transforms_test.py
@@ -1,23 +1,21 @@
-import unittest
-
 from laia.data.transforms.text import ToTensor
 from laia.utils import SymbolsTable
 
 
-class TransformerTest(unittest.TestCase):
-    def test_call_with_dict(self):
-        t = ToTensor({"a": 0, "b": 1, "<space>": 2, "<": 3})
-        x = "a < b <space> a <sp"
-        y = t(x)
-        actual = [0, 3, 1, 2, 0, None]
-        self.assertEqual(actual, y)
+def test_call_with_dict(caplog):
+    t = ToTensor({"a": 0, "b": 1, "<space>": 2, "<": 3})
+    x = "a < b <space> a <sp"
+    y = t(x)
+    assert y == [0, 3, 1, 2, 0, None]
+    assert caplog.messages.count('Could not find "<sp" in the symbols table') == 1
 
-    def test_call_with_symbols_table(self):
-        st = SymbolsTable()
-        for k, v in {"a": 0, "b": 1, "<space>": 2, "<": 3}.items():
-            st.add(k, v)
-        t = ToTensor(st)
-        x = "a < b <space> a <sp"
-        y = t(x)
-        actual = [0, 3, 1, 2, 0, None]
-        self.assertEqual(actual, y)
+
+def test_call_with_symbols_table(caplog):
+    st = SymbolsTable()
+    for k, v in {"a": 0, "b": 1, "<space>": 2, "<": 3}.items():
+        st.add(k, v)
+    t = ToTensor(st)
+    x = "a < b <space> a ö"
+    y = t(x)
+    assert y == [0, 3, 1, 2, 0, None]
+    assert caplog.messages.count('Could not find "ö" in the symbols table') == 1
