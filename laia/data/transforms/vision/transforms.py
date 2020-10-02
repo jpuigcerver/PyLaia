ab3035f8b2b0345eae33abe7481db701856e6c6a
Carlos Mocholí <carlossmocholi@gmail.com>
2020-10-02 05:22:08 +0200

    Add vision transforms tests. Fix bugs

--- laia/data/transforms/vision/transforms.py
+++ laia/data/transforms/vision/transforms.py
@@ -26,7 +26,7 @@ class Convert:
         return img.convert(self.mode)
 
     def __repr__(self) -> str:
-        return f"vision.{self.__class__.__name__}({f'mode={self.mode}' if self.mode is not None else ''})"
+        return f"vision.{self.__class__.__name__}(mode={self.mode})"
 
 
 class ToImageTensor:
@@ -50,17 +50,18 @@ class ToImageTensor:
         self.invert_transform = Invert() if invert else None
         self.random_transform = random_transform if random_transform else None
         self.resize_transform = (
-            self.resize_transform if fixed_height or fixed_width else None
+            self.resize_transform if fixed_width or fixed_height else None
         )
-        self.fixed_height = fixed_height
         self.fixed_width = fixed_width
-        self.pad_transform = self.pad_transform if min_height or min_width else None
-        self.min_height = min_width
+        self.fixed_height = fixed_height
+        self.pad_transform = self.pad_transform if min_width or min_height else None
         self.min_width = min_width
+        self.min_height = min_height
         self.pad_color = pad_color
         self.tensor_transform = ToTensor()
 
     def __call__(self, img: Image.Image) -> torch.Tensor:
+        # W x H
         assert isinstance(img, Image.Image)
         img = self.convert_transform(img)
         if self.invert_transform:
@@ -68,10 +69,10 @@ class ToImageTensor:
         if self.random_transform:
             img = self.random_transform(img)
         if self.resize_transform:
-            img = self.resize_transform(img, self.fixed_height, self.fixed_width)
+            img = self.resize_transform(img, fw=self.fixed_width, fh=self.fixed_height)
         if self.pad_transform:
             img = self.pad_transform(
-                img, self.min_height, self.min_width, pad_color=self.pad_color
+                img, mw=self.min_width, mh=self.min_height, pad_color=self.pad_color
             )
         img = self.tensor_transform(img)
         # C x H x W
@@ -79,22 +80,47 @@ class ToImageTensor:
 
     @staticmethod
     def resize_transform(
-        img: Image.Image, fh: Optional[int], fw: Optional[int]
+        img: Image.Image,
+        fw: Optional[int] = None,
+        fh: Optional[int] = None,
+        resample: int = Image.ANTIALIAS,
     ) -> Image.Image:
+        if fw and fh:
+            # resize to a fixed size
+            return img.resize((fw, fh), resample=resample)
         w, h = img.size
-        nh = fh if fh else h * fw // w
-        nw = fw if fw else w * fh // h
-        return img.resize((nw, nh), Image.BILINEAR)
+        if fw is None:
+            fw = w
+        if fh is None:
+            fh = h
+        if fw > w:
+            # upscale to a fixed width
+            return img.resize((fw, h * fw // w), resample=resample)
+        if fh > h:
+            # upscale to a fixed height
+            return img.resize((w * fh // h, fh), resample=resample)
+        # downscale
+        img.thumbnail((fw, fh), resample=resample)
+        return img
 
     @staticmethod
     def pad_transform(
-        img: Image.Image, mh: Optional[int], mw: Optional[int], pad_color: int = 0
+        img: Image.Image,
+        mw: Optional[int] = None,
+        mh: Optional[int] = None,
+        pad_color: int = 0,
     ) -> Image.Image:
         w, h = img.size
-        nh = h if mh is None or h >= mh else mh
-        nw = w if mw is None or w >= mw else mw
+        nw = w if mw is None or w > mw else mw
+        nh = h if mh is None or h > mh else mh
+        # maybe an extra pixel to the bottom
+        a, b = divmod(nw - w, 2)
+        left, right = a, a + b
+        # maybe an extra pixel to the right
+        a, b = divmod(nh - h, 2)
+        top, bottom = a, a + b
         return torchvision.transforms.functional.pad(
-            img, ((nw - w) // 2, (nh - h) // 2), fill=pad_color
+            img, (left, top, right, bottom), fill=pad_color
         )
 
     def __repr__(self) -> str:
--- tests/data/transforms/vision/vision_transforms_test.py
+++ tests/data/transforms/vision/vision_transforms_test.py
@@ -1,42 +1,139 @@
-import unittest
+import math
 
 import numpy as np
+import pytest
+import torch
 from PIL import Image
 
-from laia.data.transforms.vision import Convert, Invert
+from laia.data.transforms.vision import Convert, Invert, ToImageTensor
 
 
-class TransformerTest(unittest.TestCase):
-    def test_invert(self):
-        t = Invert()
-        x = Image.new("L", (30, 40), color=0)
-        y = t(x)
-        self.assertTupleEqual(x.size, y.size)
-        self.assertEqual(x.mode, y.mode)
-        y = np.asarray(y)
-        self.assertEqual(y.max(), 255)
-        self.assertEqual(y.min(), 255)
+def test_invert():
+    t = Invert()
+    x = Image.new("L", (30, 40), color=0)
+    y = t(x)
+    assert y.size == x.size
+    assert y.mode == x.mode
+    y = np.asarray(y)
+    assert y.max() == 255
+    assert y.min() == 255
 
-    def test_convert_greyscale(self):
-        t = Convert(mode="L")
-        x = Image.new("RGB", (30, 40), color=(127, 127, 127))
-        y = t(x)
-        self.assertTupleEqual(x.size, y.size)
-        self.assertEqual("L", y.mode)
-        y = np.asarray(y)
-        self.assertEqual(y.max(), 127)
-        self.assertEqual(y.min(), 127)
 
-    def test_convert_rgba(self):
-        t = Convert(mode="RGBA")
-        x = Image.new("L", (30, 40), color=127)
-        y = t(x)
-        self.assertTupleEqual(x.size, y.size)
-        self.assertEqual("RGBA", y.mode)
-        y = np.asarray(y)
-        # Alpha channel
-        self.assertEqual(y[:, :, -1].max(), 255)
-        self.assertEqual(y[:, :, -1].min(), 255)
-        # Color channels
-        self.assertEqual(y[:, :, :3].max(), 127)
-        self.assertEqual(y[:, :, :3].min(), 127)
+def test_convert_greyscale():
+    t = Convert(mode="L")
+    x = Image.new("RGB", (30, 40), color=(127, 127, 127))
+    y = t(x)
+    assert y.size == x.size
+    assert y.mode == "L"
+    y = np.asarray(y)
+    assert y.max(), 127
+    assert y.min(), 127
+
+
+def test_convert_rgba():
+    t = Convert(mode="RGBA")
+    x = Image.new("L", (30, 40), color=127)
+    y = t(x)
+    assert y.size == x.size
+    assert y.mode == "RGBA"
+    y = np.asarray(y)
+    # Alpha channel
+    assert y[:, :, -1].max(), 255
+    assert y[:, :, -1].min(), 255
+    # Color channels
+    assert y[:, :, :3].max(), 127
+    assert y[:, :, :3].min(), 127
+
+
+@pytest.mark.parametrize("w", [5, 6, 11, 12])
+@pytest.mark.parametrize("h", [5, 6, 11, 12])
+@pytest.mark.parametrize("fw", [2, 3, 10, 11])
+def test_resize_transform_fixed_width(w, h, fw):
+    img = Image.new("RGB", size=(w, h))
+    out = ToImageTensor.resize_transform(img, fw=fw)
+    nw, nh = out.size
+    assert nw == fw
+    assert nh in (op(h * fw / w) for op in (math.floor, math.ceil))
+
+
+@pytest.mark.parametrize("w", [5, 6, 11, 12])
+@pytest.mark.parametrize("h", [5, 6, 11, 12])
+@pytest.mark.parametrize("fh", [2, 3, 10, 11])
+def test_resize_transform_fixed_height(w, h, fh):
+    img = Image.new("L", size=(w, h))
+    out = ToImageTensor.resize_transform(img, fh=fh)
+    nw, nh = out.size
+    assert nw in (op(w * fh / h) for op in (math.floor, math.ceil))
+    assert nh == fh
+
+
+@pytest.mark.parametrize("w", [5, 6, 11, 12])
+@pytest.mark.parametrize("h", [5, 6, 11, 12])
+@pytest.mark.parametrize("fh", [2, 3, 10, 11])
+@pytest.mark.parametrize("fw", [2, 3, 10, 11])
+def test_resize_transform(w, h, fw, fh):
+    img = Image.new("L", size=(w, h))
+    out = ToImageTensor.resize_transform(img, fw=fw, fh=fh)
+    assert out.size == (fw, fh)
+
+
+@pytest.mark.parametrize("w", [5, 6, 11, 12])
+@pytest.mark.parametrize("h", [5, 6, 11, 12])
+@pytest.mark.parametrize("mw", [2, 3, 10, 11])
+def test_pad_transform_minimum_width(w, h, mw):
+    img = Image.new("L", size=(w, h))
+    out = ToImageTensor.pad_transform(img, mw=mw)
+    assert out.size == (max(w, mw), h)
+
+
+@pytest.mark.parametrize("w", [5, 6, 11, 12])
+@pytest.mark.parametrize("h", [5, 6, 11, 12])
+@pytest.mark.parametrize("mh", [2, 3, 10, 11])
+def test_pad_transform_minimum_height(w, h, mh):
+    img = Image.new("L", size=(w, h))
+    out = ToImageTensor.pad_transform(img, mh=mh)
+    assert out.size == (w, max(h, mh))
+
+
+@pytest.mark.parametrize("w", [5, 6, 11, 12])
+@pytest.mark.parametrize("h", [5, 6, 11, 12])
+@pytest.mark.parametrize("mw", [2, 3, 10, 11])
+@pytest.mark.parametrize("mh", [2, 3, 10, 11])
+def test_pad_transform(w, h, mw, mh):
+    img = Image.new("L", size=(w, h))
+    out = ToImageTensor.pad_transform(img, mw=mw, mh=mh)
+    assert out.size == (max(w, mw), max(h, mh))
+
+
+def test_to_image_tensor():
+    img = Image.new("L", size=(456, 123))  # color = 0, 255 after inverting
+
+    class Foo:
+        def __call__(self, x):
+            return x
+
+        def __repr__(self):
+            return "Foo()"
+
+    transform = ToImageTensor(
+        invert=True,
+        random_transform=Foo(),
+        fixed_height=100,
+        min_height=123,
+        min_width=456,
+        pad_color=255,
+    )
+    img = transform(img)
+    assert isinstance(img, torch.Tensor)
+    assert list(img.size()) == [1, 123, 456]
+    assert img.eq(1).all()
+    assert str(transform) == (
+        "ToImageTensor(\n"
+        "  vision.Convert(mode=L),\n"
+        "  vision.Invert(),\n"
+        "  Foo(),\n"
+        "  vision.resize_transform(),\n"
+        "  vision.pad_transform(),\n"
+        "  ToTensor()\n"
+        ")"
+    )
