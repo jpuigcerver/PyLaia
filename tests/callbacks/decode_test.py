ac949c37cba6c861cce83d968c01577496e48e5e
Carlos Mocholí <carlossmocholi@gmail.com>
2020-10-14 15:01:48 +0200

    Fix distributed Segmentation. Add tests

--- tests/callbacks/decode_test.py
+++ tests/callbacks/decode_test.py
@@ -7,7 +7,7 @@ from laia.dummies import DummyEvaluator, DummyMNISTLines, DummyTrainer
 from laia.losses.ctc_loss import transform_batch
 
 
-class __TestDecoder:
+class _TestDecoder:
     def __call__(self, batch_y_hat):
         _, xs = transform_batch(batch_y_hat)
         batch_size = len(xs)
@@ -16,7 +16,7 @@ class __TestDecoder:
 
 class __TestDecode(Decode):
     def __init__(self, img_id, hyp, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, decoder=_TestDecoder(), **kwargs)
         self.img_id = img_id
         self.hyp = re.escape(hyp)
 
@@ -42,9 +42,7 @@ class __TestDecode(Decode):
 def test_decode(tmpdir, num_processes, kwargs, img_id, hyp):
     module = DummyEvaluator()
     data_module = DummyMNISTLines(batch_size=2, va_n=12)
-    decode_callback = __TestDecode(
-        img_id, hyp, decoder=__TestDecoder(), syms=data_module.syms, **kwargs
-    )
+    decode_callback = __TestDecode(img_id, hyp, syms=data_module.syms, **kwargs)
     trainer = DummyTrainer(
         default_root_dir=tmpdir,
         limit_test_batches=3,
