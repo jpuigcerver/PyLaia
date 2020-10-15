4e51926056927b28f4f13a3f261bb61d760de5fe
Carlos Mocholí <carlossmocholi@gmail.com>
2020-10-15 14:51:25 +0200

    Move feeders out of directory. Add test

--- laia/engine/__init__.py
+++ laia/engine/__init__.py
@@ -1,5 +1,5 @@
-import laia.engine.feeders
 from laia.engine.data_module import DataModule
 from laia.engine.engine_module import EngineModule
 from laia.engine.evaluator_module import EvaluatorModule
+from laia.engine.feeder import Compose, ImageFeeder, ItemFeeder
 from laia.engine.htr_engine_module import HTREngineModule
---
+++ tests/engine/feeder_test.py
@@ -0,0 +1,34 @@
+import pytest
+import torch
+
+from laia.engine import ImageFeeder, ItemFeeder
+
+
+def test_item_feeder():
+    feeder = ItemFeeder("foo")
+    expected = "bar"
+    x = {"foo": expected, "baz": 1}
+    assert feeder(x) == expected
+
+
+def test_item_feeder_fails():
+    feeder = ItemFeeder("foo")
+    with pytest.raises(AssertionError, match="Could not find key"):
+        feeder({})
+
+
+@pytest.mark.parametrize(
+    ["x", "expected"],
+    [
+        (torch.empty(10, 20), (1, 1, 10, 20)),
+        ((torch.tensor(1), torch.empty(1, 3)), (1, 1, 1, 1)),
+    ],
+)
+def test_image_feeder(x, expected):
+    feeder = ImageFeeder()
+    assert feeder(x).data.size() == expected
+
+
+def test_view_as_4d_fails():
+    with pytest.raises(ValueError, match="Tensor with 5 dimensions"):
+        ImageFeeder.view_as_4d(torch.empty(1, 1, 1, 1, 1))
