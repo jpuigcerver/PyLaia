3bbbcfd29f41e253b9659aa859a81b1010767161
Carlos Mocholí <carlossmocholi@gmail.com>
2020-10-16 18:07:34 +0200

    Add create_model CLI tests

---
+++ tests/scripts/htr/create_model_cli_test.py
@@ -0,0 +1,60 @@
+import argparse
+import io
+import shutil
+import subprocess
+from unittest import mock
+
+import pytest
+
+import laia.common.arguments as args
+import laia.common.logging as log
+from laia.scripts.htr.create_model import get_args
+
+
+@pytest.mark.parametrize(
+    "cmd_args",
+    [
+        [],
+        # model used to train IAM-htr
+        # fmt: off
+        [
+            "--cnn_num_features", "16", "32", "48", "64", "80",
+            "--cnn_kernel_size", *(["3"] * 5),
+            "--cnn_stride", *(["1"] * 5),
+            "--cnn_dilation", *(["1"] * 5),
+            "--cnn_activations", *(["LeakyReLU"] * 5),
+            "--cnn_poolsize", *(["2"] * 3), *(["0"] * 2),
+            "--cnn_dropout", *(["0"] * 5),
+            "--cnn_batchnorm", *(["f"] * 5),
+            "--rnn_units=256",
+            "--rnn_layers=5",
+            "--adaptive_pooling=avgpool-16",
+            "--fixed_input_height=128",
+        ],
+        # fmt: on
+    ],
+)
+def test(tmpdir, cmd_args):
+    stderr_patch = mock.patch("sys.stderr", new=io.StringIO())
+
+    syms = tmpdir / "syms"
+    syms.write(None)
+
+    # first argument would be the script name, but it is ignored
+    cmd_args = ["", "1", str(syms), f"--train_path={tmpdir}"] + cmd_args
+    argv_patch = mock.patch("sys.argv", new=cmd_args)
+
+    args.parser = None
+    with stderr_patch, argv_patch:
+        parsed = get_args()
+    log.clear()
+
+    assert isinstance(parsed, argparse.Namespace)
+
+
+def test_entry_point():
+    # check that the console script entry point works
+    proc = subprocess.run(
+        [shutil.which("pylaia-htr-create-model")], stderr=subprocess.PIPE
+    )
+    assert proc.stderr.decode().startswith("usage: pylaia-htr-create-model")
