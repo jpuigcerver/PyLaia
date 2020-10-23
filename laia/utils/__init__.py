4b71cc17fec2841e1658408756c236554f855443
Carlos Mocholí <carlossmocholi@gmail.com>
2020-10-23 17:00:30 +0200

    Remove image_collage function
    Unused and untested

--- laia/utils/__init__.py
+++ laia/utils/__init__.py
@@ -1,4 +1,3 @@
 from laia.utils.checks import check_inf, check_nan
-from laia.utils.image_collage import image_collage
 from laia.utils.kaldi import ArchiveLatticeWriter, ArchiveMatrixWriter
 from laia.utils.symbols_table import SymbolsTable
