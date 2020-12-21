7993084968a14be9a985d8f8c82b9438b444af95
Carlos Mocholí <carlossmocholi@gmail.com>
2020-12-21 11:52:20 +0100

    1.0.1 → 1.0.1.dev0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_long_description():
 
 setuptools.setup(
     name="laia",
-    version="1.0.0",
+    version="1.0.1.dev0",
     author="Joan Puigcerver",
     author_email="joapuipe@gmail.com",
     maintainer="Carlos Mocholí",
