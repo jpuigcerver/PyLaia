866ea2b7102bd009ae30ceab5544510e9db451c2
starride-teklia <starride@teklia.com>
2022-12-12 10:40:03 +0000

    Remove print in version_test to solve conflict

--- tests/version_test.py
+++ tests/version_test.py
@@ -13,8 +13,6 @@ def test_wildcard_import():
 def test_versions_match():
     # check __init__ version matches setup.py (installed) version
     version = pkg_resources.require("pylaia")[0].version
-    print(version)
-    print(__version__)
     assert __version__.startswith(version)
 
 
