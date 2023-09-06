041fafc9e711f87b9bcf3a1cda75d6ec919fb9c5
Solène Tarride <starride@teklia.com>
2023-09-06 12:12:25 +0200

    Display info message only if fixed_input_height

--- laia/scripts/htr/create_model.py
+++ laia/scripts/htr/create_model.py
@@ -49,9 +49,11 @@ def run(
         "Model has {} parameters",
         sum(param.numel() for param in model.parameters()),
     )
-    log.info(
-        f"Minimum image size for this architecture: {model.get_min_valid_image_size(fixed_input_height)}"
-    )
+    if fixed_input_height:
+        log.info(
+            f"Images should be at least {model.get_min_valid_image_size()} pixels wide for convolutions. "
+            f"Smaller images will be padded."
+        )
     if save_model:
         ModelSaver(common.train_path, common.model_filename).save(
             LaiaCRNN, **vars(crnn)
